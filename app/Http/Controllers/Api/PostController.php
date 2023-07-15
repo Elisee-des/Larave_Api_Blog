bd580fad3c1268a5163049ddfb9314ded3792b83
Elisee-des <esabidani@gmail.com>
2023-07-15 17:18:13 +0000

    CRUD post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,22 +4,81 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreatePostRequest;
+use App\Http\Requests\EditPostRequest;
 use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return 'Ok ca marche';
+        try
+        {
+            return response()->json([
+                'status' => 200,
+                'message' => "Post récuperer avec success",
+                'data' => Post::all()
+            ]);
+        }catch(Exception $e)
+        {
+            return response()->json($e);
+        }
     }
 
     public function store(CreatePostRequest $request)
     {
-        $post = new Post();
+        try
+        {
+            $post = new Post();
+            $post->titre = $request->titre;
+            $post->description = $request->description;
+            $post->save();
 
-        // $post->titre = "Comment s'auto formé ?";
-        $post->description = "Biltong shank alcatra, pig chicken burgdoggen fatback.";
-        $post->save();
+            return response()->json([
+                'status' => 200,
+                'message' => "Post ajouté avec success",
+                'data' => $post
+            ]);
+        }catch(Exception $e)
+        {
+            return response()->json($e);
+        }
+    }
+
+    public function update(EditPostRequest $request, Post $post)
+    {
+        try
+        {
+            $post->titre = $request->titre;
+            $post->description = $request->description;
+            $post->save();
+
+            return response()->json([
+                'status' => 200,
+                'message' => "Post edité avec success",
+                'data' => $post
+            ]);
+        }catch(Exception $e)
+        {
+            return response()->json($e);
+        }
+    }
+
+    public function delete(Post $post)
+    {
+        try
+        {
+            $post->delete();
+
+            return response()->json([
+                'status' => 200,
+                'message' => "Post supprimé avec success",
+                'data' => $post
+            ]);
+        }catch(Exception $e)
+        {
+            return response()->json($e);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("posts", [PostController::class, "index"]);
 Route::post("posts/create", [PostController::class, "store"]);
+Route::put("posts/edit/{post}", [PostController::class, "update"]);
+Route::delete("posts/delete/{post}", [PostController::class, "delete"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
